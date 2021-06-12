dee145b95d075e6dd0e1ac3b16759a34698d2ef0
Adrian Tuzimek <tuziomek@gmail.com>
2021-06-12 17:33:47 +0200

    Add brackets

--- gerrit/image/before-entrypoint.sh
+++ gerrit/image/before-entrypoint.sh
@@ -4,11 +4,11 @@ if [ ! -d /var/gerrit/git/All-Projects.git ]
 then
   if [ -f /gerrit.config ]; then
     echo "Initializing Gerrit config ..."
-    envsubst '$GERRIT_CANONICAL_WEB_URL $LDAP_SERVER $LDAP_USERNAME $LDAP_ACCOUNTBASE' < /gerrit.config > /var/gerrit/etc/gerrit.config
+    envsubst '${GERRIT_CANONICAL_WEB_URL} ${LDAP_SERVER} ${LDAP_USERNAME} ${LDAP_ACCOUNTBASE}' < /gerrit.config > /var/gerrit/etc/gerrit.config
   fi
   if [ -f /secure.config ]; then
     echo "Initializing Gerrit secure config ..."
-    envsubst '$LDAP_PASSWORD' < /secure.config > /var/gerrit/etc/secure.config
+    envsubst '${LDAP_PASSWORD}' < /secure.config > /var/gerrit/etc/secure.config
   fi
 fi
 
