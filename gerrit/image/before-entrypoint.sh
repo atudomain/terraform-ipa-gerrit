c6f58916378786e73d3d261a67b700b52e0eb00a
Adrian Tuzimek <tuziomek@gmail.com>
2021-06-12 17:32:20 +0200

    Switch to envsubst

--- gerrit/image/before-entrypoint.sh
+++ gerrit/image/before-entrypoint.sh
@@ -4,16 +4,11 @@ if [ ! -d /var/gerrit/git/All-Projects.git ]
 then
   if [ -f /gerrit.config ]; then
     echo "Initializing Gerrit config ..."
-    cp -f /gerrit.config /var/gerrit/etc/gerrit.config
-    sed -i "s/<gerrit_canonicalWebUrl>/$GERRIT_CANONICAL_WEB_URL/" /var/gerrit/etc/gerrit.config
-    sed -i "s/<ldap_server>/$LDAP_SERVER/" /var/gerrit/etc/gerrit.config
-    sed -i "s/<ldap_username>/$LDAP_USERNAME/" /var/gerrit/etc/gerrit.config
-    sed -i "s/<ldap_accountBase>/$LDAP_ACCOUNTBASE/" /var/gerrit/etc/gerrit.config
+    envsubst '$GERRIT_CANONICAL_WEB_URL $LDAP_SERVER $LDAP_USERNAME $LDAP_ACCOUNTBASE' < /gerrit.config > /var/gerrit/etc/gerrit.config
   fi
   if [ -f /secure.config ]; then
     echo "Initializing Gerrit secure config ..."
-    cp -f /secure.config /var/gerrit/etc/secure.config
-    sed -i "s/<ldap_password>/$LDAP_PASSWORD/" /var/gerrit/etc/secure.config
+    envsubst '$LDAP_PASSWORD' < /secure.config > /var/gerrit/etc/secure.config
   fi
 fi
 
